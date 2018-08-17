9c21bfa38b79f59a486f8972dc29975bb0333c48
Ritesh Kumar <ritzforprojects@gmail.com>
2018-08-17 19:26:09 +0530

    Adding file SlicingString-02

---
+++ SlicingString-02.py
@@ -0,0 +1,12 @@
+# Acessing string characters
+
+string = 'A simple string'
+
+#first character
+print('string[0] = ',string[0])
+
+#last character
+print('string[-1] = ',string[-1])
+
+#from 3rd to 7th character
+print('string[3:7] = ',string[2:7])
