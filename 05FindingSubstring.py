e8afe22e2995cce82360b147b8343efb1edd311c
Ritesh Kumar <ritzforprojects@gmail.com>
2018-08-17 19:54:07 +0530

    adding file 05FindingSubstring

---
+++ 05FindingSubstring.py
@@ -0,0 +1,9 @@
+# Substring in a string
+
+str1 = 'the quick brown fox jumps over the lazy dog'
+str2 = input('Enter a substring : ')
+
+if str2 in str1:
+    print('Sunsting is present')
+else:
+    print('Substring not found')
