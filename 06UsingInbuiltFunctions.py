eddb38e381cefcc028467bb5d461224873beace8
Ritesh Kumar <ritzforprojects@gmail.com>
2018-08-17 20:04:24 +0530

    adding file 06UsingInbuildFunctions

---
+++ 06UsingInbuiltFunctions.py
@@ -0,0 +1,10 @@
+# Using in-build functions
+
+str1 = 'learn python'
+
+#using enumerate function
+a = list(enumerate(str1))
+print('list(enumerate(str1)) = ', a)
+
+#counting character
+print('len(str1) = ',len(str1))
